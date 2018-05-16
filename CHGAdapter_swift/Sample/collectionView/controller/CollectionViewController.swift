3b9f8f44eba596bf869cfffc1c030304e0637842
陈 海刚 <329206444@qq.com>
2018-05-17 01:32:50 +0800

    提交

--- CHGAdapter_swift/Sample/collectionView/controller/CollectionViewController.swift
+++ CHGAdapter_swift/Sample/collectionView/controller/CollectionViewController.swift
@@ -9,9 +9,8 @@
 import UIKit
 
 class CollectionViewController: UIViewController {
+    
     @IBOutlet var collectionView:UICollectionView?
-//    var adapter:CHGCollectionViewAdapter?
-//    var adapterData:CHGCollectionViewAdapterData?
     
     lazy var adapter:CHGCollectionViewAdapter = {
         () -> CHGCollectionViewAdapter in
@@ -56,18 +55,5 @@ class CollectionViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
